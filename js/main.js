d2f295b75a45431d353abb1d79def36cda340b41
isa <isaxvillain@gmail.com>
2025-11-23 07:03:38 -0800

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Auto-hide empty images
-document.querySelectorAll("img").forEach(img => {
-  img.onerror = () => img.style.display = "none";
-});
-
-// Auto-hide empty videos
-document.querySelectorAll("video").forEach(vid => {
-  vid.onerror = () => vid.style.display = "none";
-});
